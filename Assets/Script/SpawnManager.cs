444beea2500f3763d85401cba788b0c74b9b76ed
Allison Yao <allison98@gmail.com>
2021-01-22 16:57:00 -0800

    moving enemies

---
+++ Assets/Script/SpawnManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public int min = 0;
+    public int max = 10;
+    public GameObject flower;
+    public List<GameObject> enemy;
+
+    private float spaceBetweenSquares = 2.5f;
+    private float minValueX = 0;
+    private float minValueZ = 0;
+
+
+    // Start is called before the first frame update
+    void Start()
+
+    {
+        int numberOfFlowers = Random.Range(min, max);
+        for (int i = 0; i<max; i++)
+        {
+            Instantiate(flower, RandomSpawnPosition(), flower.transform.rotation);
+        }
+
+        InvokeRepeating("spawnEnemies", 2, 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void spawnEnemies() { 
+    
+        int randomInt = Random.Range(0, 2);
+        Instantiate(enemy[randomInt], RandomEnemyPosition(randomInt), enemy[randomInt].transform.rotation);
+    }
+
+    // Spawn in random Location
+    Vector3 RandomSpawnPosition()
+    {
+        float spawnPosX = minValueX + (RandomSquareIndex(-6.0f, 6.0f) * spaceBetweenSquares);
+        float spawnPosZ = minValueZ + (RandomSquareIndex(-6.0f, 6.0f) * spaceBetweenSquares);
+
+        Vector3 spawnPosition = new Vector3(spawnPosX, 0.8f, spawnPosZ);
+        return spawnPosition;
+
+    }
+
+    Vector3 RandomEnemyPosition(int direction)
+    {
+        float spawnPos = RandomSquareIndex(-14,14);
+
+        if (direction == 0)
+        {
+            Vector3 spawnPosition = new Vector3(-23f, 1.4f, spawnPos);
+            return spawnPosition;
+
+        }
+        else
+        {
+            Vector3 spawnPosition = new Vector3(spawnPos, 1.4f, 23);
+            return spawnPosition;
+
+        }
+        
+
+    }
+
+    float RandomSquareIndex(float start, float end)
+    {
+        return Random.Range(start, end);
+    }
+}
---
+++ README.md.txt
@@ -0,0 +1,6 @@
+# Unity Game Development
+
+My personal project this year is to create a small computer game using Unity. Having gone through the **Junior Programmer** tutorials offered by Unity, I've learned the basics of C# programming in Unity to start creating my own game.
+
+The idea so far is a game where the main character is to collect *flowers*. I want the look and feel of the game to be bright and happy with simple game play for now. After developing the basic gameplay, I hope to try and create my own assets!
+
